b605b955911559c94b7b8d7c9fb6359fa1fc35c7
Stefan Sprenger <stefan.sprneger@dkd.de>
2010-08-26 11:14:59 +0200

    Initial import

--- lib/constellation.rb
+++ lib/constellation.rb
@@ -5,6 +5,7 @@ module Constellation
   autoload :Config,       "constellation/config"
   autoload :DataStore,    "constellation/data_store"
   autoload :DataStores,   "constellation/data_stores"
+  autoload :Reader,       "constellation/reader"
   autoload :Runner,       "constellation/runner"
 
   class ConstellationError < StandardError
