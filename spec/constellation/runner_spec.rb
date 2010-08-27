1084abb7112d7a633492aaa9e91f3628b5cd88c6
Stefan Sprenger <stefan.sprneger@dkd.de>
2010-08-27 11:45:02 +0200

    Formatting

--- spec/constellation/runner_spec.rb
+++ spec/constellation/runner_spec.rb
@@ -7,6 +7,7 @@ describe Constellation::Runner do
   end
 
   describe "#init" do
+
     context "a ConstellationFile already exists" do
       it "should raise an error" do
         File.open("ConstellationFile", 'w') {|f| f.write("") }
@@ -16,6 +17,7 @@ describe Constellation::Runner do
         File.delete("ConstellationFile")
       end
     end
+
     it "should load the application from the Git repository given by Constellation::REPOSITORY tagged by Constellation::VERSION"
 
     context "given an error while loading the application" do
