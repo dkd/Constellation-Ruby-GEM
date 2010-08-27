8fc65a1c86e09637eedc9a31c6e958a0a5836417
Stefan Sprenger <stefan.sprneger@dkd.de>
2010-08-27 14:13:03 +0200

    Introducing spec helpers

---
+++ spec/helpers/file_helper.rb
@@ -0,0 +1,15 @@
+module FileHelpers
+
+  class << self
+
+    def create_file(filename="ConstellationFile", content="")
+      File.open(filename, "w") {|f| f.write(content) }
+    end
+
+    def destroy_file(filename="ConstellationFile")
+      File.delete(filename)
+    end
+
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,10 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'constellation'
 require 'spec'
 require 'spec/autorun'
+Dir[File.join(File.dirname(__FILE__), "helpers", "*.rb")].each do |file|
+  require file
+end
 
 Spec::Runner.configure do |config|
-  
+
 end
