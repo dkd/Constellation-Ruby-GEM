c131ee4578fa52dfd71227f1e7c81a592cff1ca3
Stefan Sprenger <stefan.sprneger@dkd.de>
2010-09-02 10:12:16 +0200

    Initial import

---
+++ spec/helpers/cassandra_helper.rb
@@ -0,0 +1,13 @@
+module CassandraHelpers
+
+  class << self
+    def drop_keyspace(keyspace_name)
+      server = Cassandra.new('system','127.0.0.1:9160')
+      begin
+        server.drop_keyspace(keyspace_name)
+      rescue
+      end
+    end
+  end
+
+end
