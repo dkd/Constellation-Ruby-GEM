6de4f40563ae32d0fdb8561a3606206e3870a34d
Stefan Sprenger <stefan.sprneger@dkd.de>
2010-09-02 10:15:24 +0200

    Formatting

--- spec/helpers/cassandra_helper.rb
+++ spec/helpers/cassandra_helper.rb
@@ -1,13 +1,15 @@
 module CassandraHelpers
 
   class << self
+
     def drop_keyspace(keyspace_name)
-      server = Cassandra.new('system','127.0.0.1:9160')
+      server = Cassandra.new("system","127.0.0.1:9160")
       begin
         server.drop_keyspace(keyspace_name)
       rescue
       end
     end
+
   end
 
 end
