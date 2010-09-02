deb9466c84d49f8afeddf21e60ae25290773f0ba
Stefan Sprenger <stefan.sprneger@dkd.de>
2010-09-02 12:06:18 +0200

    Added documentation for LogEntry

--- lib/constellation/log_entry.rb
+++ lib/constellation/log_entry.rb
@@ -1,6 +1,15 @@
 module Constellation
 
+  #
+  # Represents a log entry having the following attributes:
+  # * uuid (key)
+  # * machine
+  # * application
+  # * message
+  # * timestamp
+  #
   class LogEntry
+    attr_accessor :machine, :application, :message, :timestamp
   end
 
 end
