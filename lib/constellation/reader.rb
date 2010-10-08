3c250e77169c9120d2e20f120814e1889eb5b70c
Stefan Sprenger <stefan.sprenger@dkd.de>
2010-10-08 19:45:54 +0200

    Improved log file parsing by using polling instead of fssm

--- lib/constellation/reader.rb
+++ lib/constellation/reader.rb
@@ -20,43 +20,35 @@ module Constellation
     # Starts observing the given files for changes using the FSSM gem.
     #
     def start
-      @config.watched_files.each { |file|
-        puts "Watching for changes of #{file}"
-        @monitor.path(Dir.pwd, file) do
-          # open the file in read-only-mode pointing
-          @file = File.open(file, "a+")
-          @config = Config.instance
-          # read new log entries everytime the file gets updated
-          # and insert them into the data store
-          update { |base, relative|
-            Reader::read_log_entries(@config, @file)
-          }
-
-        end
-      }
 
       puts ""
       puts ">> Starting file observation"
-      @monitor.run
+
+      Reader::read_log_entries(@config, File.open("logs", "a+"))
     end
 
     class << self
 
       def read_log_entries(config, file)
-        begin
-          while(line = file.readline)
-            log_entry = LogEntry.new(line)
-            begin
-              config.data_store.insert(log_entry)
-            rescue Constellation::InvalidLogFormatError => e
-              new_system_error(config, e)
+        while(true)
+
+          begin
+            while(line = file.readline)
+              log_entry = LogEntry.new(line)
+              begin
+                config.data_store.insert(log_entry)
+              rescue Constellation::InvalidLogFormatError => e
+                new_system_error(config, e)
+              end
             end
+          # rescue from several errors that may occur due to an invalid log format
+          # but should not appear in order to avoid performance issues
+          rescue EOFError => e
+          rescue FSSM::CallbackError => e
+            new_system_error(config, e)
           end
-        # rescue from several errors that may occur due to an invalid log format
-        # but should not appear in order to avoid performance issues
-        rescue EOFError => e
-        rescue FSSM::CallbackError => e
-          new_system_error(config, e)
+
+          sleep(2)
         end
       end
 
--- spec/performance/reliability_spec.rb
+++ spec/performance/reliability_spec.rb
@@ -19,7 +19,7 @@ require 'cassandra/0.7'
 #
 
 # seconds to wait before counting the number of inserted log entries
-BUFFER_TIME = 5
+BUFFER_TIME = 3
 # example log message
 LOG_MESSAGE = "Sep 17 17:02:02 www1 php5: I failed.\n"
 
@@ -31,11 +31,6 @@ describe "Constellation reliability tests" do
       sleep(seconds)
     }
     sleep(BUFFER_TIME)
-    trigger_fssm(log_file_name)
-  end
-
-  def trigger_fssm(log_file_name)
-    file = File.open(log_file_name, "a") { |f| f.write(LOG_MESSAGE) }
   end
 
   before(:each) do
@@ -48,38 +43,143 @@ describe "Constellation reliability tests" do
 
   context "one log entry per write action" do
 
-    context "within 5 seconds" do
-      before(:each) do
+    before(:each) do
+      begin
         @server.clear_keyspace! if @server.count_range(:logs) > 0
+      rescue
+      end
+    end
+
+    context "within 1 second" do
+      it "should handle 5 actions" do
+        create_log_entries(@log_file_name, 5, 0.2)
+        @server.count_range(:logs).should == 5
       end
 
+      it "should handle 10 actions" do
+        create_log_entries(@log_file_name, 10, 0.1)
+        @server.count_range(:logs).should == 10
+      end
+
+      it "should handle 20 actions" do
+        create_log_entries(@log_file_name, 20, 0.05)
+        @server.count_range(:logs).should == 20
+      end
+
+      it "should handle 50 actions" do
+        create_log_entries(@log_file_name, 50, 0.002)
+        @server.count_range(:logs).should == 50
+      end
+
+      it "should handle 100 actions" do
+        create_log_entries(@log_file_name, 100, 0.001)
+        @server.count_range(:logs).should == 100
+      end
+
+      it "should handle 200 actions" do
+        create_log_entries(@log_file_name, 200, 0.0005)
+        @server.count_range(:logs, { :count => 200 }).should == 200
+      end
+
+      it "should handle 400 actions" do
+        create_log_entries(@log_file_name, 400, 0.00025)
+        @server.count_range(:logs, { :count => 400 }).should == 400
+      end
+
+      it "should handle 1000 actions" do
+        create_log_entries(@log_file_name, 1000, 0.0001)
+        @server.count_range(:logs, { :count => 1000 }).should == 1000
+      end
+
+      it "should handle 2000 actions" do
+        create_log_entries(@log_file_name, 2000, 0.00005)
+        @server.count_range(:logs, { :count => 2000 }).should == 2000
+      end
+    end
+
+    context "within 5 seconds" do
+      before(:each) do
+        pending
+      end
       it "should handle 5 actions" do
         create_log_entries(@log_file_name, 5, 1)
-        @server.count_range(:logs).should >= 5
+        @server.count_range(:logs).should == 5
       end
 
       it "should handle 10 actions" do
         create_log_entries(@log_file_name, 10, 0.5)
-        @server.count_range(:logs).should >= 10
+        @server.count_range(:logs).should == 10
       end
 
       it "should handle 20 actions" do
         create_log_entries(@log_file_name, 20, 0.25)
-        @server.count_range(:logs).should >= 20
+        @server.count_range(:logs).should == 20
       end
 
       it "should handle 50 actions" do
         create_log_entries(@log_file_name, 50, 0.1)
-        @server.count_range(:logs).should >= 50
+        @server.count_range(:logs).should == 50
       end
 
       it "should handle 100 actions" do
         create_log_entries(@log_file_name, 100, 0.05)
-        @server.count_range(:logs).should >= 100
+        @server.count_range(:logs).should == 100
+      end
+
+      it "should handle 200 actions" do
+        create_log_entries(@log_file_name, 200, 0.025)
+        @server.count_range(:logs, { :count => 200 }).should == 200
+      end
+
+      it "should handle 400 actions" do
+        create_log_entries(@log_file_name, 400, 0.0125)
+        @server.count_range(:logs, { :count => 400 }).should == 400
+      end
+
+      it "should handle 1000 actions" do
+        create_log_entries(@log_file_name, 1000, 0.005)
+        @server.count_range(:logs, { :count => 1000 }).should == 1000
+      end
+
+      it "should handle 2000 actions" do
+        create_log_entries(@log_file_name, 2000, 0.0025)
+        @server.count_range(:logs, { :count => 2000 }).should == 2000
+      end
+
+      it "should handle 3000 actions" do
+        create_log_entries(@log_file_name, 3000, 0.00167)
+        @server.count_range(:logs, { :count => 3000 }).should == 3000
       end
     end
 
     context "within 10 seconds" do
+      before(:each) do
+        pending
+      end
+      it "should handle 10 actions" do
+        create_log_entries(@log_file_name, 10, 1)
+        @server.count_range(:logs).should == 10
+      end
+
+      it "should handle 20 actions" do
+        create_log_entries(@log_file_name, 20, 0.5)
+        @server.count_range(:logs).should == 20
+      end
+
+      it "should handle 40 actions" do
+        create_log_entries(@log_file_name, 40, 0.25)
+        @server.count_range(:logs).should == 40
+      end
+
+      it "should handle 100 actions" do
+        create_log_entries(@log_file_name, 100, 0.1)
+        @server.count_range(:logs).should == 100
+      end
+
+      it "should handle 200 actions" do
+        create_log_entries(@log_file_name, 200, 0.05)
+        @server.count_range(:logs, { :count => 200 }).should == 200
+      end
     end
 
     context "within 30 seconds" do
