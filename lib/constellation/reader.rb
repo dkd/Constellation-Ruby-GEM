08043fb0ce6b21ac60ac25b81849da8707ea013f
Stefan Sprenger <stefan.sprenger@dkd.de>
2010-10-08 19:47:01 +0200

    Removed FSSM and refactored code

--- lib/constellation/reader.rb
+++ lib/constellation/reader.rb
@@ -1,5 +1,3 @@
-require 'fssm'
-
 module Constellation
 
   #
@@ -10,9 +8,7 @@ module Constellation
   # is getting used by FSSM.
   #
   class Reader
-
     def initialize(config)
-      @monitor    = FSSM::Monitor.new
       @config     = config
     end
 
@@ -20,52 +16,46 @@ module Constellation
     # Starts observing the given files for changes using the FSSM gem.
     #
     def start
-
       puts ""
       puts ">> Starting file observation"
 
-      Reader::read_log_entries(@config, File.open("logs", "a+"))
+      read_log_entries(@config, File.open("logs", "a+"))
     end
 
-    class << self
+    def read_log_entries(config, file)
+      while(true)
 
-      def read_log_entries(config, file)
-        while(true)
-
-          begin
-            while(line = file.readline)
-              log_entry = LogEntry.new(line)
-              begin
-                config.data_store.insert(log_entry)
-              rescue Constellation::InvalidLogFormatError => e
-                new_system_error(config, e)
-              end
+        begin
+          while(line = file.readline)
+            log_entry = LogEntry.new(line)
+            begin
+              config.data_store.insert(log_entry)
+            rescue Constellation::InvalidLogFormatError => e
+              new_system_error(config, e)
             end
-          # rescue from several errors that may occur due to an invalid log format
-          # but should not appear in order to avoid performance issues
-          rescue EOFError => e
-          rescue FSSM::CallbackError => e
-            new_system_error(config, e)
           end
-
-          sleep(2)
+        # rescue from several errors that may occur due to an invalid log format
+        # but should not appear in order to avoid performance issues
+        rescue EOFError => e
+        rescue FSSM::CallbackError => e
+          new_system_error(config, e)
         end
-      end
 
-      #
-      # Log errors that get raised while reading the log file
-      #
-      def new_system_error(config, error)
-        log_entry             = Constellation::LogEntry.new
-        log_entry.machine     = "system"
-        log_entry.application = "Constellation"
-        log_entry.timestamp   = Time.now.to_i
-        log_entry.message     = "A new exception got raised: #{error.inspect}"
-        config.data_store.insert(log_entry)
+        sleep(2)
       end
-
     end
 
+    #
+    # Log errors that get raised while reading the log file
+    #
+    def new_system_error(config, error)
+      log_entry             = Constellation::LogEntry.new
+      log_entry.machine     = "system"
+      log_entry.application = "Constellation"
+      log_entry.timestamp   = Time.now.to_i
+      log_entry.message     = "A new exception got raised: #{error.inspect}"
+      config.data_store.insert(log_entry)
+    end
   end
 
 end
