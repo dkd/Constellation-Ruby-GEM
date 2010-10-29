9be5e595f40461c577c2e3f70ffeb0f4b50d813c
Stefan Sprenger <stefan.sprenger@dkd.de>
2010-10-29 10:46:35 +0200

    Mute console output while running specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,8 @@ Dir[File.join(File.dirname(__FILE__), "helpers", "*.rb")].each do |file|
   require file
 end
 
-Constellation::UserInterface.mute!
-
 RSpec.configure do |config|
-
+  config.before(:each) do
+    Constellation::UserInterface.mute!
+  end
 end
