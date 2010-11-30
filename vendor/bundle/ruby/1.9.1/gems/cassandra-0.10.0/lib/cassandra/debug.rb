d39df146b0f7b1d4ba01ede1d109ac4ed0413eeb
Stefan Sprenger <stefan.sprenger@dkd.de>
2010-11-30 11:18:45 +0100

    Preps for non-RVM usage

--- vendor/bundle/ruby/1.9.1/gems/cassandra-0.10.0/lib/cassandra/debug.rb
+++
@@ -1,9 +0,0 @@
-
-require 'pp'
-
-class CassandraThrift::Cassandra::Client
-  def send_message(*args)
-    pp args
-    super
-  end
-end
--- vendor/bundle/ruby/1.9.1/gems/titan-0.1.0/lib/titan.rb
+++
@@ -1,5 +0,0 @@
-require "titan/version"
-
-module Titan
-  autoload :Thread,   "titan/thread"
-end
